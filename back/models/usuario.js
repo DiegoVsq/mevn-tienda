e7f29ae62f0bdcc1d101d2ebba121d5c083b1291
DiegoVsq <dvasquezar@unprg.edu.pe>
2023-06-02 11:54:38 -0500

    first

---
+++ back/models/usuario.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UsuarioSchema = Schema({
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    rol: { type: String, required: true },
+    password: { type: String, required: true },
+    estado: { type: Boolean, default: true, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('usuario', UsuarioSchema);
